72934a34c36b77f911677093be19ddc5aefaa543
iamgauraav <iamgauraav@gmail.com>
2022-03-05 16:00:56 +0530

    Adding UC3 : code to flip through loop till 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,21 +1,36 @@
 #!/bin/bash -x
 
 read -p "Enter the number of  flip: " num
+
 headCount=0
 tailCount=0
 count=0
 
-for ((i=0;i<=n;i++))
+while (($headCount<21 && $tailCount<21 ))
 do
-	randomCheck=$(($(($RANDOM%10))%2))
+	flip=$(($((RANDOM%10))%2))
 	if [ $flip -eq 1 ]
 	then
-		echo "HEADS"
+		echo "Heads"
 		headCount=$(($headCount+1))
 	else
-		echo "TAILS"
+		echo "Tail"
 		tailCount=$(($tailCount+1))
+		count=$(($count+1))
 	fi
 done
-echo "Heads=$headCount"
-echo "Tails=$tailCount"
+
+echo "$heads=$headCount"
+echo "$tails=$tailCount"
+
+if [ $headCount -gt $tailCount ]
+then	
+	winHead=$(($headCount - $tailCount))
+	echo "Heads Won and win difference is $winHead"
+elif [ $headCount -lt $tailCount ]
+then
+	winTail=$(($tailCount - $headCount))
+	echo "Tails won and twin difference is $winTail"
+else
+	echo "Tie"
+fi
